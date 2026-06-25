04e82949e6cee408f709fb85cc433d8b9cc2331a
dxvampi <dxvampi@proton.me>
2026-06-25 18:12:03 +0200

    Removed unused imports

--- src/main/java/ic/dxvampi/inventoryCommands/commands/CraftingTableCommand.java
+++ src/main/java/ic/dxvampi/inventoryCommands/commands/CraftingTableCommand.java
@@ -2,14 +2,8 @@ package ic.dxvampi.inventoryCommands.commands;
 
 import ic.dxvampi.inventoryCommands.commands.base.BaseCommand;
 import ic.dxvampi.inventoryCommands.commands.base.CommandErrors;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public class CraftingTableCommand extends BaseCommand {
 
